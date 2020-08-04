83ed822666ea2f679530db1f40908ea20a69bff0
Balasumanth <beldebalasumanth6@gmail.com>
2020-08-04 16:17:12 +0530

    components Part 2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -7,7 +7,8 @@ function Menu(props) {
 	const renderMenuItem = ({item, index}) => {
 		return(
 			<ListItem key={index} title={item.name} subtitle={item.description}
-					hideChevron={true} leftAvatar={{source: require('./images/uthappizza.png')}} />
+					hideChevron={true} leftAvatar={{source: require('./images/uthappizza.png')}}
+					onPress={() => props.onPress(item.id)} />
 		);
 	}
 
