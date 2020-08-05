cca80604cc52984db635cc32361883e022f0568a
Balasumanth <beldebalasumanth6@gmail.com>
2020-08-05 14:50:12 +0530

    Buttons and Icons

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -6,7 +6,7 @@ import {LEADERS} from '../shared/leaders';
 function History () {
 
 	return (
-		<Card title='Our History'> 
+		<Card title='Our History' titleStyle={{color: '#512DA8'}}> 
 			<Text style={{margin: 10}}>
 				Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
 			</Text>
@@ -43,7 +43,7 @@ class AboutUs extends Component {
 		return (
 			<ScrollView>
 				<History />
-				<Card title='Corporate Leadership'>
+				<Card title='Corporate Leadership' titleStyle={{color: '#512DA8'}}>
 					<FlatList data={this.state.leaders} renderItem={RenderLeaders} 
 						keyExtractor={item => item.id.toString()} />
 				</Card>
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,20 +4,23 @@ import Home from './HomeComponent';
 import ContactUs from './ContactComponent';
 import AboutUs from './AboutComponent';
 import Dishdetail from './DishdetailComponent';
-import { View, Platform } from 'react-native';
+import { View, Platform, Image, StyleSheet, SafeAreaView, ScrollView, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import Constants from 'expo-constants';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { Icon } from 'react-native-elements';
 
 const MenuNavigator = createAppContainer(createStackNavigator({
 	
-	Menu: { screen: Menu },
+	Menu: { screen: Menu, 
+		navigationOptions: ({ navigation }) => ({
+			headerLeft: <Icon name='menu' size={24} color='white' onPress={() => navigation.toggleDrawer()} />
+		}) },
 	Dishdetail: { screen: Dishdetail }
 }, {
 	initialRouteName: 'Menu', 
-	defaultNavigationOptions: {
+	defaultNavigationOptions: ({navigation}) => ({
 		headerStyle: {
 			backgroundColor: '#511DA8'
 		},
@@ -25,53 +28,72 @@ const MenuNavigator = createAppContainer(createStackNavigator({
 		headerTitleStyle: {
 			color: '#fff'
 		}
-	}
+	})
 }))
 
 const HomeNavigator = createAppContainer(createStackNavigator({
 	
 	Home: { screen: Home },
 }, {
-	defaultNavigationOptions: {
+	defaultNavigationOptions: ({ navigation }) => ({
 		headerStyle: {
 			backgroundColor: '#512DA8'
 		},
 		headerTintColor: '#fff',
 		headerTitleStyle: {
 			color: '#fff'
-		}
-	}
+		},
+		headerLeft: <Icon name='menu' size={24} color='white' onPress={() => navigation.toggleDrawer()} />
+	}) 
 }))
 
 const ContactUsNavigator = createAppContainer(createStackNavigator({
 	
 	ContactUs: { screen: ContactUs },
 }, {
-	defaultNavigationOptions: {
+	defaultNavigationOptions: ({ navigation }) => ({
 		headerStyle: {
 			backgroundColor: '#512DA8'
 		},
 		headerTintColor: '#fff',
 		headerTitleStyle: {
 			color: '#512DA8'
-		}
-	}
+		},
+		headerLeft: <Icon name='menu' size={24} color='white' onPress={() => navigation.toggleDrawer()} />
+	})
 }))
 
 const AboutUsNavigator = createAppContainer(createStackNavigator({
 	
 	AboutUs: { screen: AboutUs },
 }, {
-	defaultNavigationOptions: {
+	defaultNavigationOptions: ({ navigation }) => ({
 		headerStyle: {
 			backgroundColor: '#512DA8'
 		},
 		headerTintColor: '#fff',
 		headerTitleStyle: {
 			color: '#512DA8'
-		}
-	}
+		},
+		headerLeft: <Icon name='menu' size={24} color='white' onPress={() => navigation.toggleDrawer()} />
+	})
 }))
+
+const CustomDrawerContentComponent = (props) => (
+	<ScrollView>
+		<SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }} >
+			<View style={styles.drawerHeader} >
+				<View style={{flex: 1}}>
+					<Image source={require('./images/logo.png')} style={styles.drawerImage} />
+				</View>
+				<View style={{flex: 2}}>
+					<Text style={styles.drawerHeaderText} >Ristorante Con Fusion</Text>
+				</View> 
+			</View>
+			<DrawerItems {...props} />
+		</SafeAreaView>
+	</ScrollView>
+);
 
 const MainNavigator = createAppContainer(createDrawerNavigator({
 
@@ -79,32 +101,45 @@ const MainNavigator = createAppContainer(createDrawerNavigator({
 		screen: HomeNavigator,
 		navigationOptions: {
 			title: 'Home',
-			drawerLabel: 'Home'
+			drawerLabel: 'Home',
+			drawerIcon: ({ tintColor }) => (
+				<Icon name='home' type='font-awesome' size={24} color={tintColor} />
+			)
 		}
 	},
 	AboutUs: {
 		screen: AboutUsNavigator,
 		navigationOptions: {
 			title: 'About Us',
-			drawerLabel: 'About Us'
+			drawerLabel: 'About Us',
+			drawerIcon: ({ tintColor }) => (
+				<Icon name='info-circle' type='font-awesome' size={24} color={tintColor} />
+			)
 		}
 	},
 	Menu: {
 		screen: MenuNavigator,
 		navigationOptions: {
 			title: 'Menu',
-			drawerLabel: 'Menu'
+			drawerLabel: 'Menu',
+			drawerIcon: ({ tintColor }) => (
+				<Icon name='list' type='font-awesome' size={24} color={tintColor} />
+			)
 		}
 	},
 	ContactUs: {
 		screen: ContactUsNavigator,
 		navigationOptions: {
 			title: 'Contact Us',
-			drawerLabel: 'Contact Us'
+			drawerLabel: 'Contact Us',
+			drawerIcon: ({ tintColor }) => (
+				<Icon name='address-card' type='font-awesome' size={22} color={tintColor} />
+			)
 		}
 	}
 }, {
-	drawerBackgroundColor: '#D1C4E9'
+	drawerBackgroundColor: '#D1C4E9',
+	contentComponent: CustomDrawerContentComponent
 }))
 
 class Main extends Component {
@@ -117,5 +152,30 @@ class Main extends Component {
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	drawerHeader: {
+		backgroundColor: '#512DA8',
+		height: 120,
+		alignItems: 'center',
+		justifyContent: 'center',
+		flex: 1,
+		flexDirection: 'row'
+	},
+	drawerHeaderText: {
+		color: 'white',
+		fontSize: 18	,
+		fontWeight: 'bold',
+		margin: 16
+	},
+	drawerImage: {
+		margin: 22,
+		width: 68,
+		height: 50
+	}
+})
 
 export default Main;
