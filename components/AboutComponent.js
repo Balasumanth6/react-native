b4f4b45a0a3b4ec027fac4c8fb6693d952958df3
Balasumanth <beldebalasumanth6@gmail.com>
2020-08-14 01:17:58 +0530

    Animations & Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { Card, Title, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
 	return {
@@ -53,10 +54,12 @@ class AboutUs extends Component {
 		else if (this.props.leaders.errMess) {
 			return (
 				<ScrollView>
-					<History />
-					<Card title='Corporate Leadership' >
-						<Text>{this.props.leaders.errMess}</Text>
-					</Card>
+					<Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+						<History />
+						<Card title='Corporate Leadership' >
+							<Text>{this.props.leaders.errMess}</Text>
+						</Card>
+					</Animatable.View>
 				</ScrollView>	
 			);
 		}
@@ -64,11 +67,13 @@ class AboutUs extends Component {
 		else {
 			return (
 				<ScrollView>
-					<History />
-					<Card title='Corporate Leadership' titleStyle={{color: '#512DA8'}}>
-						<FlatList data={this.props.leaders.leaders} renderItem={RenderLeaders} 
-							keyExtractor={item => item.id.toString()} />
-					</Card>
+					<Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+						<History />
+						<Card title='Corporate Leadership' titleStyle={{color: '#512DA8'}}>
+							<FlatList data={this.props.leaders.leaders} renderItem={RenderLeaders} 
+								keyExtractor={item => item.id.toString()} />
+						</Card>
+					</Animatable.View>
 				</ScrollView>
 			);
 		}
